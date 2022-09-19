cb94473e2daf3eb86e53ebfaf5f8a627005680f6
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-19 21:30:32 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # atcoder-js
+
+# なんでjest v28なの？
+
+jest v29からはnode v12が切り捨てられたから。
+AtCoderのJavaScriptはv12.1.6。
---
+++ src/nya.js
@@ -0,0 +1,18 @@
+function main(input = "") {
+  const [a, b, c, d] = input.split(/\s/).map(Number);
+  return `${(a + b) * (c - d)}\nTakahash`;
+}
+
+if (require.main === module) {
+  console.log(main(require("fs").readFileSync(0, "utf8")));
+} else {
+  [
+    ["1 2 5 3", "6\nTakahashi"],
+    ["10 -20 30 -40", "-700\nTakahashi"],
+    ["100 100 100 -100", "40000\nTakahashi"],
+  ].forEach(([input, output], i) => {
+    test(`example ${i}`, () => {
+      expect(main(input)).toBe(output);
+    });
+  })
+}
