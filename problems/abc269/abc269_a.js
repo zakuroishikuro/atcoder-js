8cd62135f716bbf85703b66905c5230560633344
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-20 01:40:49 +0900

    Anyway Takahashi

--- problems/abc269/abc269_a.js
+++ problems/abc269/abc269_a.js
@@ -2,8 +2,8 @@
 // url: https://atcoder.jp/contests/abc269/tasks/abc269_a
 // date: 2022-09-19T16:26:22.979Z
 function main(input = '') {
-  const [a, b, ...items] = input.split(/\s/).map(Number);
-  return 'a';
+  const [a, b, c, d] = input.split(/\s/).map(Number);
+  return `${(a + b) * (c - d)}\nTakahashi`;
 }
 
 if (require.main === module) {
