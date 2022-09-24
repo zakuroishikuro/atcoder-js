1087c15b63c44ec1e66520306e64c74c9e0f56be
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-24 13:10:38 +0900

    sovled

---
+++ problems/abc061/abc061_b.js
@@ -0,0 +1,37 @@
+// B - Counting Roads
+// https://atcoder.jp/contests/abc061/tasks/abc061_b
+// 2022-09-24T02:39:13.370Z
+//Object.getOwnPropertyNames(Math).forEach((n) => globalThis[n] = Math[n]);
+
+// 「同じ 2 つの都市を結ぶ道路は、1 本とは限りません。」
+// ↑これ読めてなくて勝手にsetでやって失敗した
+
+function main(input = "") {
+  const rows = input.split(/\n/).map((row) => row.split(/\s/).map(Number));
+  const [verticeSize] = rows.shift();
+
+  const graph = [...Array(verticeSize)].map(() => 0);
+  rows.forEach(([a, b]) => {
+    graph[--a]++;
+    graph[--b]++;
+  });
+  return graph.join("\n");
+}
+
+if (process.env.NODE_ENV != "test") {
+  console.log(main(require("fs").readFileSync(0, "utf8").trim()));
+} else {
+  test("stub", () => {
+    expect().toBe();
+  });
+
+  [
+    ["4 3\n1 2\n2 3\n1 4", "2\n2\n1\n1"],
+    ["2 5\n1 2\n2 1\n1 2\n2 1\n1 2", "5\n5"],
+    ["8 8\n1 2\n3 4\n1 5\n2 8\n3 7\n5 2\n4 1\n6 8", "3\n3\n2\n2\n2\n1\n1\n2"],
+  ].forEach(([input, output], i) => {
+    test(`example #${i + 1}`, () => {
+      expect(`${main(input)}`).toBe(output);
+    });
+  });
+}
