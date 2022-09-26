0451e19bf5cfe37233bf4a7dbcf2ab612837b057
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-26 22:18:12 +0900

    途中まで

--- problems/abc115/abc115_d.js
+++ problems/abc115/abc115_d.js
@@ -5,16 +5,39 @@ const { floor, ceil, abs, sign, sqrt, log, log2, log10 } = Math;
 const _vector = (s, fn = (v) => v) => s.split(/\s/).map(fn);
 const _matrix = (s, fn) => s.split(/\n/).map((r) => _vector(r, fn));
 
-const cook = (level) => {
+const _cook = (level) => {
   if (level == 0) return "P";
-  const sub = cook(level - 1);
+  const sub = _cook(level - 1);
   return `_${sub}P${sub}_`;
+};
+
+const cook = (level, x) => {
+  if (x == 1) return 0;
+
+  if (x <= )
+
+  
+
+  const [subPatty, subBurger] = cook(level - 1);
+  const patty = 2 * subPatty + 1;
+  const burger = 2 * subBurger + 3;
+  return [patty, burger];
 };
 
 function main(input = "") {
   const [n, x] = _vector(input, Number);
-  const burger = cook(n);
-  return (burger.slice(-x).match(/P/g) || []).length;
+
+  if (x <= 1) return 0;
+
+  [patty, burger] = cook(n - 2);
+  if (x < burger
+  
+
+  if (x == 0)
+
+  const 
+
+  return //burger; //(burger.slice(-x).match(/P/g) || []).length;
 }
 
 if (process.env.NODE_ENV != "test") {
@@ -31,16 +54,24 @@ if (process.env.NODE_ENV != "test") {
     const input = examples[idx - 1][0];
     console.log(`----- 🐈 example #${idx}:\n${input}\n----- output:\n${main(input)}\n-----\n`);
   } else {
+    test("_cook", () => {
+      expect(_cook(0)).toBe("P");
+      expect(_cook(1)).toBe("_PPP_");
+      expect(_cook(2)).toBe("__PPP_P_PPP__");
+      expect(_cook(4)).toBe("____PPP_P_PPP__P__PPP_P_PPP___P___PPP_P_PPP__P__PPP_P_PPP____");
+      expect(_cook(5)).toBe("_____PPP_P_PPP__P__PPP_P_PPP___P___PPP_P_PPP__P__PPP_P_PPP____P____PPP_P_PPP__P__PPP_P_PPP___P___PPP_P_PPP__P__PPP_P_PPP_____");
+    });
+
     test("cook", () => {
-      expect(cook(0)).toBe("P");
-      expect(cook(1)).toBe("_PPP_");
-      expect(cook(2)).toBe("__PPP_P_PPP__");
-      expect(cook(4)).toBe("____PPP_P_PPP__P__PPP_P_PPP___P___PPP_P_PPP__P__PPP_P_PPP____");
-      expect(cook(5)).toBe("_____PPP_P_PPP__P__PPP_P_PPP___P___PPP_P_PPP__P__PPP_P_PPP____P____PPP_P_PPP__P__PPP_P_PPP___P___PPP_P_PPP__P__PPP_P_PPP_____");
+      expect(cook(0)).toEqual([1, 1]);
+      expect(cook(1)).toEqual([3, 5]);
+      expect(cook(2)).toEqual([7, 13]);
+      expect(cook(4)).toEqual([31, 61]);
+      expect(cook(5)).toEqual([63, 125]);
     });
 
     examples.forEach(([input, output], i) => {
-      test(`example #${i + 1}`, () => {
+      test.skip(`example #${i + 1}`, () => {
         expect(`${main(input)}`).toBe(output);
       });
     });
