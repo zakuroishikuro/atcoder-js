269f1734c99a1ceb7e06e42324717834ac73c649
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-30 00:37:40 +0900

    無駄に短くした

--- problems/abc267/abc267_b.js
+++ problems/abc267/abc267_b.js
@@ -2,23 +2,19 @@
 // https://atcoder.jp/contests/abc267/tasks/abc267_b
 // 2022-09-29T14:50:09.071Z
 
-function main(input = "") {
-  const n = parseInt(input, 2);
-  if (input[0] == "1") return "No";
-
-  const cols = [
-    
-    0b0_000_001_000,
-    0b0_001_000_000,
-    0b0_100_000_100,
-    0b1_000_100_000,
-    0b0_010_000_010,
-    0b0_000_010_000,
-    0b0_000_000_001,
-  ].map((col) => Math.sign(col & n));
-
-  return cols.join("").match(/10+1/) ? "Yes" : "No";
-}
+const main = (s = "") => {
+  return !+s[0] && [
+      s[6],
+      s[3],
+      s[1] | s[7],
+      s[0] | s[4],
+      s[2] | s[8],
+      s[5],
+      s[9],
+    ].join("").match(/10+1/)
+    ? "Yes"
+    : "No";
+};
 
 if (process.env.NODE_ENV != "test") {
   console.log(main(require("fs").readFileSync(0, "utf8").trim()));
