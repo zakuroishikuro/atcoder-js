79a48fd9605199c3dddcaaa0c17cbc7e0b28d94f
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-10-06 23:12:02 +0900

    solved

---
+++ problems/abc237/abc237_a.js
@@ -0,0 +1,44 @@
+// A - Not Overflow
+// https://atcoder.jp/contests/abc237/tasks/abc237_a
+// 2022-10-05T15:27:08.167Z
+
+function main(input = '') {
+  const n = +input;
+  const max = 2 ** 31;
+  return -max <= n && n < max ? "Yes" : "No";
+}
+
+function parse(s = '', f = Number) {
+  const m = f ? (s) => s.split(/\s/).map(f) : (s) => s.split(/\s/);
+  return /\n.+\n/s.test(s) ? s.split(/\n/).map(m) : m(s);
+}
+
+// prettier-ignore
+Object.defineProperties(Object.prototype,{log:{get:require.main==module?function(){return this}:function(){console.log(this);return this}}});
+if (process.env.NODE_ENV != 'test') console.log(main(require('fs').readFileSync(0, 'utf8').trim()));
+
+//----------------//
+//----- test -----//
+//----------------//
+if (process.env.NODE_ENV == 'test') {
+  const examples = [
+    ["10","Yes"],["-9876543210","No"],["483597848400000","No"]
+  ];
+
+  if (process.env.NEKO == 'cat') {
+    const idx = process.argv[2] || 1;
+    const input = examples[idx - 1][0];
+    console.log(`----- 🐈 example #${idx}:`);
+    console.log(`${input}\n----- output:\n${main(input)}\n-----\n`);
+  } else {
+    examples.forEach(([input, output], i) => {
+      test(`example #${i + 1}`, () => {
+        expect(`${main(input)}`).toBe(output);
+      });
+    });
+
+    test('stub', () => {
+      expect().toBe();
+    });
+  }
+}
