c2e73b89ed9d6936960d0323f90483e253e06f85
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-10-08 17:39:13 +0900

    テンプレ変更

--- _src/_template.js
+++ _src/_template.js
@@ -3,20 +3,23 @@
 // /*timestamp*/
 
 // prettier-ignore
+if (process.env.NODE_ENV != 'test') console.log(solve(require('fs').readFileSync(0, 'utf8').trim()).toString());
 Object.defineProperties(Object.prototype, { log: { get: require.main == module ? function () { return this } : function () { console.log(this); return this } } });
-
 const _vector = (s, f = v => v) => s.split(/\s/).map(f); //一次元配列
 const _matrix = (s, f = v => v) => s.split(/\s/).map(r => _vector(r, f)); //二次元配列
 
-function main(input = '') {
+//　　　∧＿∧
+//　　　(　･ω･)＝つ≡つ
+//　　　(っ　≡つ＝つ
+//　　　./　　　) ﾊﾞﾊﾞﾊﾞﾊﾞ
+//　　　( /￣∪
+
+function solve(input = '') {
   const [A, B, ...data] = _vector(input, Number);
   //const [[_A, _B], ...data] = _matrix(input, Number); 
 
   return A;
 }
-
-if (process.env.NODE_ENV != 'test')
-  console.log(main(require('fs').readFileSync(0, 'utf8').trim()));
 
 //----------------//
 //----- test -----//
@@ -34,7 +37,7 @@ if (process.env.NODE_ENV == 'test') {
   } else {
     examples.forEach(([input, output], i) => {
       test(`example #${i + 1}`, () => {
-        expect(`${main(input)}`).toBe(output);
+        expect(solve(input).toString()).toBe(output);
       });
     });
 
