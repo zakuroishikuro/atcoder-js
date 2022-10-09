e494c2c7cc69857fcc73269817333d02e68f68fe
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-10-09 23:46:57 +0900

    テンプレ変更

--- _src/_template.ts
+++ _src/_template.ts
@@ -2,21 +2,26 @@
 // /*url*/
 // /*timestamp*/
 
-if (!process.env.VITEST) console.log(solve(require("fs").readFileSync(0, "utf8").trim().toString()));
-
+//
 // ＼( 'ω')／ウオオオオアアーーーッッ！！！
+//
 export function solve(input: string) {
   const [A, B] = input.split(/\s/).map(Number);
   //const [[_N, _K], ...data] = input.split(/\n/).map((r) => r.split(/\s/).map(Number));
-  return N;
+  return A;
 }
 
+// deno-fmt-ignore
+if (!process.env.VITEST) console.log(solve(require("fs").readFileSync(0, "utf8").trim().toString()));
+
+// ------------------------
 // ＼( 'ω')／テストッッ！！！
+// ------------------------
 if (process.env.VITEST) {
   const { test, expect } = import.meta.vitest;
   test.each([
     /*examples*/
-  ])("example %#: %j -> %j", (input, expected) => {
+  ])("example %#:\n---input---\n%s\n-----------", (input, expected) => {
     expect(solve(input).toString()).toBe(expected);
   });
 }
