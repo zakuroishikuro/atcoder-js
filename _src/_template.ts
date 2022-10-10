6717501c25f86324b166a37888c82b504308e458
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-10-10 13:04:23 +0900

    テストケースの作成はExcelでやろう

--- _src/_template.ts
+++ _src/_template.ts
@@ -19,9 +19,7 @@ if (!process.env.VITEST) console.log(solve(require("fs").readFileSync(0, "utf8")
 // ------------------------
 if (process.env.VITEST) {
   const { test, expect } = import.meta.vitest;
-  const seed = (s = 0xC0FFEE) => (h = 1000, l = 0) => (s ^= s << 13, s ^= s >> 17, s ^= s << 5, l + (Math.abs(s) % (h + 1 - l)));
-  const randNums = (len = 10000, h?: number, l?: number, rand = seed()) => () => [...Array(len)].map(() => rand(h, l));
-
+  
   test.each([
     /*examples*/
   ])("example %#:\n---input---\n%s\n-----------", (input, expected) => {
