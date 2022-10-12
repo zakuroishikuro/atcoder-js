a0fbbcd435318fc561ed4f3b50a474f90f511ef0
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-10-12 23:04:54 +0900

    おそうじ

--- problems/abc001/abc001_1.ts
+++ problems/abc001/abc001_1.ts
@@ -2,18 +2,19 @@
 // https://atcoder.jp/contests/abc001/tasks/abc001_1
 // 2022-09-28T14:02:53.981Z
 
-function main(input = "") {
+function main(input: string) {
   const [h1, h2] = input.split(/\s/).map(Number);
   return h1 - h2;
 }
 
-if (process.env.NODE_ENV != "test") console.log(main(require("fs").readFileSync(0, "utf8").trim()));
+if (process.env.NODE_ENV != "test")
+  console.log(main(require("fs").readFileSync(0, "utf8").trim()));
 else {
   test.each([
     ["15\n10", "5"],
     ["0\n0", "0"],
     ["5\n20", "-15"],
-  ])("example %#", ([input, output], i) => {
-    expect(`${main(input)}`).toBe(output);
+  ])("example %#", (input, expected) => {
+    expect(main(input).toString()).toBe(expected);
   });
 }
