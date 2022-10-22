181638c1bc3dbea99e2d2b685a745efdfee7006d
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-10-22 12:44:17 +0900

    pg battle(topsic)の準備
    とりあえずコピーは手動でやる

---
+++ problems-topsic/_template.ts
@@ -0,0 +1,19 @@
+// /*subject*/
+// /*url*/
+// /*timestamp*/
+
+export function main(input: string) {
+  const [A, B] = input.split(/\s/).map(Number);
+  //const [[_N, _K], ...data] = input.split(/\n/).map((r) => r.split(/\s/).map(Number));
+  return A;
+}
+
+if (require.main == module) console.log(main(require("fs").readFileSync(0, "utf8").trim()).toString());
+
+if (process.env.NODE_ENV == "test") {
+  test.each([
+    /*examples*/
+  ])("example %#", (input, expected) => {
+    expect(main(input).toString()).toBe(expected);
+  });
+}
