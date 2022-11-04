5249047734d4a7213f8b27f717d0fc7a743a86c5
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-11-05 01:41:42 +0900

    テンプレ修正

--- _src/_template.ts
+++ _src/_template.ts
@@ -2,14 +2,16 @@
 // /*url*/
 // /*timestamp*/
 
+const parseVector = (s: string) => s.split(/\s/).map((s) => (isNaN(+s) ? s : Number(s)));
+const parseMatrix = (s: string) => s.split(/\n/).map(parseVector);
+
 export function main(input: string) {
-  const [A, B] = input.split(/\s/).map(Number);
-  //const [[N, M], ...data] = input.split(/\n/).map((r) => r.split(/\s/).map(Number));
+  const [A, B] = parseVector(input) as number[];
   return A;
 }
 
 if (require.main == module) {
-  if (process.send) console.log(main(require("fs").readFileSync(0, "utf8").trim()).toString());
+  if (process.send) console.log(main(require("fs").readFileSync(0, "utf8").trim()) + "");
   else require("child_process").fork(__filename, { execArgv: ["--stack-size=99900"] });
 }
 
@@ -17,6 +19,6 @@ if (process.env.NODE_ENV == "test") {
   test.each([
     /*examples*/
   ])("example %#", (input, expected) => {
-    expect(main(input).toString()).toBe(expected);
+    expect(main(input) + "").toBe(expected);
   });
 }
