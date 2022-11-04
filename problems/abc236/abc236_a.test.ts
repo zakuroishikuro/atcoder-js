2f8a1a7eb9bf0436adfe31d640e0f1c74c3dd1c7
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-11-05 01:45:39 +0900

    新しいテンプレ使ってみる

--- problems/abc236/abc236_a.test.ts
+++ problems/abc236/abc236_a.test.ts
@@ -2,19 +2,18 @@
 // https://atcoder.jp/contests/abc236/tasks/abc236_a
 // 2022-11-04T14:23:59.131Z
 
+const parseVector = (s: string) => s.split(/\s/).map((s) => (isNaN(+s) ? s : Number(s)));
+const parseMatrix = (s: string) => s.split(/\n/).map(parseVector);
+
 export function main(input: string) {
-  const [S, a, b] = input.split(/\s/);
-  const chars = [...S];
+  const [[...chars], a, b] = parseVector(input) as [string, ...number[]];
   [chars[+a - 1], chars[+b - 1]] = [chars[+b - 1], chars[+a - 1]];
   return chars.join("");
 }
 
 if (require.main == module) {
-  if (!process.argv.includes("f")) {
-    require("child_process").fork(__filename, ["f"], { execArgv: ["--stack-size=99900"] });
-  } else {
-    console.log(main(require("fs").readFileSync(0, "utf8").trim()).toString());
-  }
+  if (process.send) console.log(main(require("fs").readFileSync(0, "utf8").trim()) + "");
+  else require("child_process").fork(__filename, { execArgv: ["--stack-size=99900"] });
 }
 
 if (process.env.NODE_ENV == "test") {
