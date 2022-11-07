0a7298ffd939c5f75fee3a4595cd492f37661cb8
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-11-07 11:53:43 +0900

    テンプレ修正

--- _src/_template.ts
+++ _src/_template.ts
@@ -2,13 +2,13 @@
 // /*url*/
 // /*timestamp*/
 
-export function solve(input: string): number | string {
+export function main(input: string): number | string {
   const [A, B] = input.split(/\s/).map(Number);
   return input;
 }
 
 if (require.main == module) { //ここから読む必要なし
-  if (process.send) console.log(solve(require("fs").readFileSync(0, "utf8").trim()));
+  if (process.send) console.log(main(require("fs").readFileSync(0, "utf8").trim()));
   else require("child_process").fork(__filename, { execArgv: ["--stack-size=99900"] });
 }
 
@@ -16,6 +16,6 @@ if (process.env.NODE_ENV == "test") { // テスト
   test.each([
     /*examples*/
   ])("example %#", (input, expected) => {
-    expect(`${solve(input)}`).toBe(expected);
+    expect(`${main(input)}`).toBe(expected);
   });
 }
