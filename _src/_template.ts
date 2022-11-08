f6a05a840e4f9eeffcb3ecc6a33e997011729adb
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-11-08 12:24:01 +0900

    ローカルはts-nodeによるテスト、AtCoderではchild_processのみ動くよう変更

--- _src/_template.ts
+++ _src/_template.ts
@@ -7,7 +7,7 @@ export function main(input: string): number | string {
   return input;
 }
 
-if (require.main == module) {
+if (__filename.endsWith(".js")) {
   if (process.send) console.log(main(require("fs").readFileSync(0, "utf8").trim()));
   else require("child_process").fork(__filename, { execArgv: ["--stack-size=99900"] });
 } else {
