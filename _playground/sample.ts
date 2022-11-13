e43edb385c8fbe9c356030227197730753e33f6f
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-11-13 19:00:53 +0900

    あそび

--- _playground/sample.ts
+++ _playground/sample.ts
@@ -2,6 +2,10 @@ const DEBUG = process.env.NODE_ENV === "test";
 Object.assign(Object.prototype,{log<T>(this:T,msg="log"){if(DEBUG)console.log(`[${msg}] ${this}`);return this}}) // prettier-ignore
 declare global { interface Object{log<T>(this:T):T} } // prettier-ignore
 
+declare global {
+  
+}
+
 export function main() {
   "abcd".log().slice(1).log().slice(1).log();
 }
