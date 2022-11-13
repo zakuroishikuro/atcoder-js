69ba43e1bc69b2a9a80cd2557cfbeb5ae29e7377
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-11-13 11:13:43 +0900

    必要になったので連想配列で作ったグラフも表示できるようにした

--- custom-visualizer.js
+++ custom-visualizer.js
@@ -39,6 +39,44 @@ module.exports = (register, helpers) => {
   });
 
   register({
+    id: "showGraphDict",
+    getExtractions(data, collector) {
+      if (Array.isArray(data)) return;
+      if (typeof data !== "object" || data == undefined) return;
+      const current = data["current"];
+      const next = data["next"];
+
+      collector.addExtraction({
+        priority: 1000,
+        id: "showGraphDict",
+        name: "グラフ表示(object)",
+        extractData() {
+          return helpers.asData({
+            kind: { graph: true },
+            nodes: Object.keys(data)
+              .filter((k) => !Number.isNaN(+k))
+              .map((pos) => {
+                return { id: `${pos}`, color: current == pos ? "red" : "#D2E5FF" };
+              }),
+            edges: Object.entries(data)
+              .filter(([k]) => !Number.isNaN(+k))
+              .flatMap(([pos, adjList]) =>
+                adjList.map((adj) => {
+                  const color = current == pos ? (next == adj ? "red" : "orange") : "#2B7CE9";
+                  return {
+                    from: `${pos}`,
+                    to: `${adj}`,
+                    color,
+                  };
+                }),
+              ),
+          });
+        },
+      });
+    },
+  });
+
+  register({
     id: "showGrid",
     getExtractions(data, collector) {
       if (!Array.isArray(data)) return;
