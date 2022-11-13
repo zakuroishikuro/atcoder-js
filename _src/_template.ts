67f99db7afa074b4658ad781ab89e7a874dca3e0
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-11-13 14:50:47 +0900

    テンプレ修正

--- _src/_template.ts
+++ _src/_template.ts
@@ -1,26 +1,28 @@
 // /*subject*/
 // /*url*/
 // /*timestamp*/
-const LOCAL = !__filename.includes("imojudge");
-Object.assign(Object.prototype,{log<T>(this:T,msg="log"){if(LOCAL)console.log(`[${msg}] ${this}`);return this}}) // prettier-ignore
-interface Object{log<T>(this:T):T} // prettier-ignore
 
-function main(input: string): number | string {
+export function main(input: string): number | string {
   const [A, B] = input.split(/\s/).map(Number);
   return input;
 }
 
-if (!LOCAL) {
+// === ここから読む必要なし ===
+const DEBUG = process.env.NODE_ENV === "test";
+Object.assign(Object.prototype,{log<T>(this:T,msg="log"){if(DEBUG)console.log(`[${msg}] ${this}`);return this}}) // prettier-ignore
+declare global { interface Object{log<T>(this:T):T} } // prettier-ignore
+if (!DEBUG) {
   if (process.send) console.log(main(require("fs").readFileSync(0, "utf8").trim()));
   else require("child_process").fork(__filename, { execArgv: ["--stack-size=99900"] });
 }
 
-if (LOCAL) {
+if (DEBUG) {
   [
     /*examples*/
   ].forEach(([input, e], i) => {
     const a = main(input).toString();
-    console.log(`example #${i}: `, a === e ? "success !" : "failed !");
-    if (a !== e) console.log(a);
+    const failed = a !== e;
+    console.log(`example #${i}: `, failed ? "failed !" : "success !");
+    if (failed) console.error(JSON.stringify({ input, e, a }, null, "  "));
   });
 }
