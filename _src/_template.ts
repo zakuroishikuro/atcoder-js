f95e8ba25466b56f05e81291cd7b64332c57754d
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-11-13 14:48:38 +0900

    テンプレ修正

--- _src/_template.ts
+++ _src/_template.ts
@@ -1,20 +1,26 @@
 // /*subject*/
 // /*url*/
 // /*timestamp*/
+const LOCAL = !__filename.includes("imojudge");
+Object.assign(Object.prototype,{log<T>(this:T,msg="log"){if(LOCAL)console.log(`[${msg}] ${this}`);return this}}) // prettier-ignore
+interface Object{log<T>(this:T):T} // prettier-ignore
 
-export function main(input: string): number | string {
+function main(input: string): number | string {
   const [A, B] = input.split(/\s/).map(Number);
   return input;
 }
 
-if (__filename.endsWith(".js")) {
+if (!LOCAL) {
   if (process.send) console.log(main(require("fs").readFileSync(0, "utf8").trim()));
   else require("child_process").fork(__filename, { execArgv: ["--stack-size=99900"] });
-} else {
+}
+
+if (LOCAL) {
   [
     /*examples*/
   ].forEach(([input, e], i) => {
     const a = main(input).toString();
-    if (a != e) console.error(`failed !`, { i, input, e, a });
+    console.log(`example #${i}: `, a === e ? "success !" : "failed !");
+    if (a !== e) console.log(a);
   });
 }
